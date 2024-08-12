1856993c90700bc9eecf1bc50191a23898955833
BasavarajChannapattan <107638236+BasavarajChannapattan@users.noreply.github.com>
2024-08-12 22:25:07 +0530

    new endpoint
    Added new api

--- Rest-Api/handler.js
+++ Rest-Api/handler.js
@@ -1,8 +1,33 @@
-exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
-  };
+const axios = require('axios');
+
+module.exports.getStatus = async (event) => {
+  try {
+    const response = await axios.get('https://simple-grocery-store-api.glitch.me/status');
+    return {
+      statusCode: response.status,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: error.response ? error.response.status : 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
 };
+
+module.exports.getProducts = async (event)=>{
+  try{
+    const res = await axios.get("https://simple-grocery-store-api.glitch.me/products?results=20")
+    return{
+      statusCode:res.status,
+      body:JSON.stringify(res.data),
+    };
+  }
+  catch(error)
+  {
+    return{
+      statusCode:error.res?error.res.status:500,
+      body:JSON.stringify({message:error.message}),
+    };
+  }
+}
